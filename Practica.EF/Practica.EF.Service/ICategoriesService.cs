5b3481a2bba1d8460330326714a96c59ff54bef1
vicki <vickimathurin94@gmail.com>
2022-10-10 12:02:04 -0300

    First commit
    Creacion proyecto web API y servicio

---
+++ Practica.EF/Practica.EF.Service/ICategoriesService.cs
@@ -0,0 +1,10 @@
+﻿using Practica.EF.Entities;
+using System.Collections.Generic;
+
+namespace Practica.EF.Service
+{
+    public interface ICategoriesService
+    {
+        List<Categories> GetCategories();
+    }
+}
---
+++ Practica.EF/Practica.EF.WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Practica.EF.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
